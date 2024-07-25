e79157bacbd6a873365f369922622682370f3d6c
UnrulyJuli3 <sid3rdark@gmail.com>
2024-07-24 19:45:51 -0700

    this code sucks

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using RT.PropellerApi;
+using RT.Util;
+
+namespace KtaneManualRenderPropeller
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PropellerUtil.RunStandalone(PathUtil.AppPathCombine("settings.json"), new ManualRenderModule());
+        }
+    }
+}
